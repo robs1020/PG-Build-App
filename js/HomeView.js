bc2fef8fe7763054755e9b4e32f3bb2889f30ef5
Robert Sethi <robs1020@gmail.com>
2013-08-22 11:19:06 -0400

    remove that scope

--- js/HomeView.js
+++ js/HomeView.js
@@ -3,6 +3,7 @@ var HomeView = function(store){
 	this.initialize = function(){
 		//Define a div wrapper for the view. The div wrapper is used to attach events.
 		this.el = $('<div/>');
+		this.el.on('keyup', '.search-key', this.findByName);
 	};
 
 	this.render = function() {
@@ -22,6 +23,8 @@ var HomeView = function(store){
 	        }
 	    });
 	};
+
+	this.initialize();
 	
 };
 
